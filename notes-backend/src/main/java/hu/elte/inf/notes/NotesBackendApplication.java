aafc74bb99336b807df9f36c75d128a4bcc5423b
Abel Sohajda <abels.elte@proton.me>
2023-06-01 15:08:17 +0200

    add backend

---
+++ notes-backend/src/main/java/hu/elte/inf/notes/NotesBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.inf.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesBackendApplication.class, args);
+	}
+
+}
---
+++ notes-backend/src/test/java/hu/elte/inf/notes/NotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.inf.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
